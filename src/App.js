8d1861941ae8a0471a3e45f7b05c714f7fc34b3b
KevinsCodeStorage <89494039+KevinsCodeStorage@users.noreply.github.com>
2022-02-09 17:27:29 -0500

    attempt to fix nav

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import './App.css';
 import NavBar from './components/NavBar';
 import StoreListing from './components/StoreListing';
 
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
       <NavBar />
       <StoreListing />
     </div>
