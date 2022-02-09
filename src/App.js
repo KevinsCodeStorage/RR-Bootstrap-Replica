b9dddb827697d00ef21088035ae576e5a683f184
KevinsCodeStorage <89494039+KevinsCodeStorage@users.noreply.github.com>
2022-02-09 11:33:06 -0500

    basic codding done, need to fix navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import NavBar from './components/NavBar';
+import StoreListing from './components/StoreListing';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <NavBar />
+      <StoreListing />
     </div>
   );
 }
